b2f76905844cad7e297f561c6a7e03f6ef9a159f
Julian <81520713+Juliandev02@users.noreply.github.com>
2022-03-27 12:50:16 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import random
 import array
+import sqlite3 as sql
+from Cryptodome.Hash import SHAKE256
+import itertools as it
+counter = it.count()
+
 
 random.seed()
 
@@ -10,6 +15,15 @@ LOWERCASE = ['a', 'b', 'c', 'd', 'e', 'f', "g", "h", "i", "j", "k", "l", "m", "n
 UPPERCASE = ['A', 'B', 'C', 'D', 'E', 'F', "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X"]
 
 COMBINED = DIGITS+LOWERCASE+UPPERCASE
+
+# hashed_pro = sql.connect("keys_pro.db")
+# raw_pro = sql.connect("keys_pro_raw.db")
+
+# hashed_enterprise = sql.connect("keys_enterprise.db")
+# raw_enterprise = sql.connect("keys_enterprise_raw.db")
+
+hashed_developer = sql.connect("keys_developer.db")
+raw_developer = sql.connect("keys_developer_raw.db")
 
 while True:
     rand_digit1 = random.choice(DIGITS)
@@ -72,4 +86,26 @@ while True:
 
     full_key = key1 + "-" + key2 + "-" + key3 + "-" + key4
 
-    print(full_key, file=open("output.txt", "a"))
+    ByteKeys = str.encode(full_key)
+    KeyHashing = SHAKE256.new()
+    KeyHashing.update(ByteKeys)
+    HashedKey = KeyHashing.read(26).hex()
+
+    # Uncomment to Create new Keys
+
+    # hashed_pro.execute(f"INSERT INTO keys (KEY) VALUES ('{HashedKey}')")
+    # raw_pro.execute(f"INSERT INTO keys (KEY) VALUES ('{full_key}')")
+    # hashed_pro.commit()
+    # raw_pro.commit()
+    
+    # hashed_enterprise.execute(f"INSERT INTO keys (KEY) VALUES ('{HashedKey}')")
+    # raw_enterprise.execute(f"INSERT INTO keys (KEY) VALUES ('{full_key}')")
+    # hashed_enterprise.commit()
+    # raw_enterprise.commit()
+    
+    hashed_developer.execute(f"INSERT INTO keys (KEY) VALUES ('{HashedKey}')")
+    raw_developer.execute(f"INSERT INTO keys (KEY) VALUES ('{full_key}')")
+    hashed_developer.commit()
+    raw_developer.commit()
+
+    print(next(counter))
